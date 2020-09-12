191852bf4409961fc36b3d489ac4641a649f426b
Robert O'Connor <robert@mdcalc.com>
2020-09-12 18:47:44 -0400

    Add initial frontend with register and login forms

---
+++ client/src/components/loginForm.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+
+class LoginForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      loggedIn: false
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(e) {
+    e.preventDefault();
+    const field = e.target.getAttribute('id');
+    if (field === 'login-email') {
+      this.setState({ 'email': e.target.value });
+    } else {
+      this.setState({ 'password': e.target.value });
+    }
+  }
+
+  async handleSubmit(e) {
+    try {
+      e.preventDefault();
+      const loginInfo = this.state;
+      console.log(loginInfo);
+
+      const preJSONifiedRes = await fetch('http://localhost:7001/v1/login', {
+        method: 'POST',
+        body: JSON.stringify(loginInfo),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      const res = await preJSONifiedRes.json();
+      console.log(res);
+      if (res.status === 200) {
+        localStorage.setItem('authToken', JSON.stringify(res.data.token));
+        localStorage.setItem('user', JSON.stringify(res.data.user));
+        this.setState({ loggedIn: true });
+        console.log("Login successful!");
+      } else {
+        throw new Error(JSON.stringify(res.message));
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async processForgotPassword() {
+    console.log(this.state.email);
+    try {
+      const body = { email: this.state.email };
+      const forgotPasswordResponse = await fetch('http://localhost:7001/v1/forgotPassword', {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      console.log(await forgotPasswordResponse.json());
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  render() {
+    console.log(this.state);
+    if (this.state.loggedIn) return (<Redirect to='/mainpage' />);
+
+    return (
+      <div className="login-form-container">
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="login-email">Email</label>
+          <input
+            id="login-email"
+            value={this.state.email}
+            placeholder="Email"
+            onChange={this.handleInputChange}
+          /><br></br>
+          <label htmlFor="login-password">Password</label>
+          <input
+            id="login-password"
+            value={this.state.password}
+            placeholder="Password"
+            onChange={this.handleInputChange}
+          /><br></br>
+          <button>Login</button>
+        </form>
+        <button onClick={() => this.processForgotPassword()}>Forgot Password</button>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
---
+++ client/src/components/registerForm.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+
+class RegisterForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      loggedIn: false
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(e) {
+    e.preventDefault();
+    const field = e.target.getAttribute('id');
+    if (field === 'sign-up-email') {
+      this.setState({ 'email': e.target.value });
+    } else {
+      this.setState({ 'password': e.target.value });
+    }
+  }
+
+  async handleSubmit(e) {
+    try {
+      e.preventDefault();
+      const signUpInfo = this.state;
+      console.log(signUpInfo);
+
+      const preJSONifiedRes = await fetch('http://localhost:7001/v1/register', {
+        method: 'POST',
+        body: JSON.stringify(signUpInfo),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      const res = await preJSONifiedRes.json();
+      console.log(res);
+      if (res.status === 200) {
+        localStorage.setItem('authToken', res.token);
+        localStorage.setItem('user', JSON.stringify(res.data));
+        this.setState({ loggedIn: true });
+        console.log("Registration complete!");
+      } else {
+        throw new Error(JSON.stringify(res.message));
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  render() {
+    console.log(this.state);
+    if (this.state.loggedIn) return (<Redirect to='/mainpage' />);
+
+    return (
+      <div className="sign-up-form-container">
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="sign-up-email">Email</label>
+          <input
+            id="sign-up-email"
+            value={this.state.email}
+            placeholder="Email"
+            onChange={this.handleInputChange}
+          /><br></br>
+          <label htmlFor="sign-up-password">Password</label>
+          <input
+            id="sign-up-password"
+            value={this.state.password}
+            placeholder="Password"
+            onChange={this.handleInputChange}
+          /><br></br>
+          <button>Sign Up</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default RegisterForm;
