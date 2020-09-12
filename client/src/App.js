f8c550d9b8ad7151d2fb9cc61ab877aa5b6138ec
Robert O'Connor <robert@mdcalc.com>
2020-09-12 19:00:03 -0400

    Update App component with basic forms

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  Route,
+  HashRouter
+} from 'react-router-dom';
+
+import RegisterForm from './components/registerForm.js';
+import LoginForm from './components/loginForm.js';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <div className="App">
+        <h1>Standard Project!</h1>
+        <div>Hi</div>
+        <Route path="/login" component={LoginForm} />
+        <Route path="/register" component={RegisterForm} />
+      </div>
+    </HashRouter>
   );
 }
 
