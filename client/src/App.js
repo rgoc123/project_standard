cb2e210c9d29685a710b8251bf22321a9b949baf
Robert O'Connor <robertgoconnor2@gmail.com>
2020-09-12 20:43:21 -0400

    Add initial redux actions and reducers

--- client/src/App.js
+++ client/src/App.js
@@ -3,25 +3,46 @@ import {
   Route,
   HashRouter
 } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import Header from './components/header.js'
 import RegisterForm from './components/registerForm.js'
 import LoginForm from './components/loginForm.js'
 import MainPage from './components/mainPage.js'
 
+import configureStore from './store/store'
+
 import './App.css'
 
 function App() {
+  let store = configureStore();;
+  document.addEventListener('DOMContentLoaded', () => {
+    if (window.currentUser) {
+      const preloadedState = { session: { currentUser: window.currentUser } };
+      store = configureStore(preloadedState);
+      window.getState = store.getState;
+      window.dispatch = store.dispatch;
+      delete window.currentUser;
+    } else {
+      store = configureStore();
+      // debugger
+      window.getState = store.getState;
+      window.dispatch = store.dispatch;
+    }
+  });
+
   return (
-    <HashRouter>
-      <div className="App">
-        <Header />
-        <div>Hi</div>
-        <Route path="/" component={MainPage} />
-        <Route path="/login" component={LoginForm} />
-        <Route path="/register" component={RegisterForm} />
-      </div>
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <div className="App">
+          <Header />
+          <div>Hi</div>
+          <Route path="/" component={MainPage} />
+          <Route path="/login" component={LoginForm} />
+          <Route path="/register" component={RegisterForm} />
+        </div>
+      </HashRouter>
+    </Provider>
   );
 }
 
---
+++ client/src/actions/sessionActions.js
@@ -0,0 +1,49 @@
+import * as APIUtil from '../util/sessionApiUtil';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+export const CLEAR_ERRORS = 'CLEAR_ERRORS';
+
+//async actions
+export const receiveCurrentUser = currentUser => {
+  return {
+    type: RECEIVE_CURRENT_USER,
+    currentUser
+  };
+};
+
+export const receiveSessionErrors = sessionErrors => {
+  // return {
+  //   type: RECEIVE_SESSION_ERRORS,
+  //   sessionErrors
+  // };
+};
+
+export const clearErrors = () => {
+  // return {
+  //   type: CLEAR_ERRORS
+  // };
+};
+
+//thunk actions
+export const signup = user => dispatch => {
+  return(APIUtil.signup(user).then(user => (
+    dispatch(receiveCurrentUser(user))
+  ), err => (
+    dispatch(receiveSessionErrors(err.responseJSON))
+  )));
+};
+
+export const login = user => dispatch => {
+  return(APIUtil.login(user).then(user => (
+    dispatch(receiveCurrentUser(user))
+  ), err => (
+    dispatch(receiveSessionErrors(err.responseJSON))
+  )));
+};
+
+export const logout = () => dispatch => {
+  return(APIUtil.logout().then(user => (
+    dispatch(receiveCurrentUser(null))
+  )));
+};
