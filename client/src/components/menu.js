fc3a9cf5af349acaff5f357b4331a88c384ac9bc
Robert O'Connor <robertgoconnor2@gmail.com>
2020-12-21 13:24:33 -0500

    Style menu buttons

--- client/src/components/menu.js
+++ client/src/components/menu.js
@@ -4,9 +4,9 @@ export default function Menu({ activeTab, setActiveTab }) {
   const getStyle = (tab) => activeTab === tab ? 'active' : 'inactive'
 
   return (
-    <div>
+    <div className="menu">
       {['learn', 'cook', 'workout', 'watch'].map(tab => (
-        <button className={getStyle(tab)}
+        <button className={'menu-button ' + getStyle(tab)}
           onClick={() => setActiveTab(tab)}
           key={tab} >
           {tab[0].toUpperCase() + tab.slice(1)}
